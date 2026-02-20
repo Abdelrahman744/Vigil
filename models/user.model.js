7e017ffcd510f11daacf1a3d27934aa148096cf4
Abdelrahman744 <ww.ab472005@gmail.com>
2026-02-20 10:59:49 +0200

    added authentication

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    
+    this.password = await bcrypt.hash(this.password, 12);
+    next(); // This 'next' refers to the parameter above
+});
+
+// Method to check password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model('User', userSchema);
--- routes/monitor.routes.js
+++ routes/monitor.routes.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import { signup, login } from '../controllers/auth.controller.js';
+import { protect } from '../middlewares/auth.middleware.js';
 import { pingWebsite,getAllLogs,clearLogs,addTarget,getTargets,deleteTarget,toggleTarget,getTargetStats } from '../controllers/monitor.controller.js';
 
 const router = express.Router();
+
+
+// Public Auth Routes
+router.post('/signup', signup);
+router.post('/login', login);
+
+// Protected Monitoring Routes
+router.use(protect);
 
 
 router.post('/ping', pingWebsite);
